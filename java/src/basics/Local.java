db32007154ba8642e47cb5ee957a5dbc5730c9e4
thurakamanasa <manasathuraka68@gmail.com>
2023-03-05 12:12:16 +0530

    Basics

---
+++ java/src/basics/Local.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Local {
+
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println(a);
+		int b=4;
+		System.out.println(b);
+
+	}
+
+}
---
+++ java/src/basics/Static.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Static {
+	
+	static int a=20;
+	public static void main(String[] args) {
+		
+		System.out.println(a);
+	}
+
+
+	
+}
